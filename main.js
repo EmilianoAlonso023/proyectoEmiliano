9de00c62693301cde7847703cf6dfe4296a7ec45
Emiliano <alonsoemiliano62@gmail.com>
2022-08-01 23:11:58 -0300

    commit agosto

---
+++ main.js
@@ -0,0 +1,11 @@
+let edad = 33;
+let nombre = "Emiliano"
+const PI = 3.14;
+let añoNacimiento = 1988; 
+let miEdad = 2022 - añoNacimiento;
+
+let nombreCompleto = nombre + "" + apellido
+
+let miNombre = prompt("como te llamas?");
+
+console.log("Mañana va a llover");
